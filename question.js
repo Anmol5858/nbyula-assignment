1709da3967c8515fda9f7138fc77411ccd7a52f3
Anmol5858 <anmoloberoi12.01.ao@gmail.com>
2022-06-19 11:44:22 +0530

    first commit

---
+++ question.js
@@ -0,0 +1,59 @@
+
+let questions = [
+  {
+  numb: 1,
+  question: "#include statement must be written.",
+  answer: "Before main()",
+  options: [
+    "After main()",
+    "Before main()",
+    "Before any scanf/printf",
+    "It can be written anywhere"
+  ]
+},
+  {
+  numb: 2,
+  question: "What does CSS stand for?",
+  answer: "Cascading Style Sheet",
+  options: [
+    "Common Style Sheet",
+    "Colorful Style Sheet",
+    "Computer Style Sheet",
+    "Cascading Style Sheet"
+  ]
+},
+  {
+  numb: 3,
+  question: "Which of the datatypes have size that is variable?",
+  answer: "struct",
+  options: [
+    "int",
+    "char",
+    "struct",
+    "float"
+  ]
+},
+  {
+  numb: 4,
+  question: "What does SQL stand for?",
+  answer: "Structured Query Language",
+  options: [
+    "Stylish Question Language",
+    "Stylesheet Query Language",
+    "Statement Question Language",
+    "Structured Query Language"
+  ]
+},
+  {
+  numb: 5,
+  question: "Which of these is necessary to specify at time of array initialization?",
+  answer: "Row",
+  options: [
+    "Row",
+    "Column",
+    "Both Row and Column",
+    "None of the mentioned"
+  ]
+},
+
+];
